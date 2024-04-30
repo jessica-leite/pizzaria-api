8719af1ded420613c289903f53e6e8cfd26ce6a7
Jessica Leite <jessica.mleite@gmail.com>
2024-04-30 20:04:46 -0300

    Arquivos iniciais

---
+++ src/main/java/com/api/pizzaria/PizzariaApplication.java
@@ -0,0 +1,13 @@
+package com.api.pizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaApplication.class, args);
+	}
+
+}
